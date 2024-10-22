d8dcd98c19158742c837a9ae4c360a9b072ea1d1
Nicolaj Blach Jensen <nico188f@gmail.com>
2024-10-22 15:58:36 +0200

    FIX: Made CRUD routes async

--- api/Controllers/MeetingRoomController.cs
+++ api/Controllers/MeetingRoomController.cs
@@ -6,6 +6,7 @@ using api.Data;
 using api.Dtos.MeetingRoom;
 using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace api.Controllers
 {
@@ -20,15 +21,15 @@ namespace api.Controllers
         }
 
         [HttpGet]
-        public IActionResult GetAll(){
-            var meetingRooms = _context.MeetingRooms.ToList();
+        public async Task<IActionResult> GetAll(){
+            var meetingRooms = await _context.MeetingRooms.ToListAsync();
 
             return Ok(meetingRooms);
         }
 
         [HttpGet("{id}")]
-        public IActionResult GetById([FromRoute] int id){
-            var meetingRoom = _context.MeetingRooms.Find(id);
+        public async Task<IActionResult> GetById([FromRoute] int id){
+            var meetingRoom = await _context.MeetingRooms.FindAsync(id);
 
             if(meetingRoom == null){
                 return NotFound();
@@ -38,18 +39,18 @@ namespace api.Controllers
         }
 
         [HttpPost]
-        public IActionResult Create([FromBody] CreateMeetingRoomRequest createMeetingRoomRequest) {
+        public async Task<IActionResult> Create([FromBody] CreateMeetingRoomRequest createMeetingRoomRequest) {
             var meetingRoom = createMeetingRoomRequest.ToMeetingRoom();
 
-            _context.MeetingRooms.Add(meetingRoom);
-            _context.SaveChanges();
+            await _context.MeetingRooms.AddAsync(meetingRoom);
+            await _context.SaveChangesAsync();
 
             return CreatedAtAction(nameof(GetById), new { id = meetingRoom.Id }, meetingRoom);
         }
 
         [HttpPut("{id}")]
-        public IActionResult Update([FromRoute] int id, [FromBody] UpdateMeetingRoomRequest updateMeetingRoomRequest) {
-            var meetingRoom = _context.MeetingRooms.Find(id);
+        public async Task<IActionResult> Update([FromRoute] int id, [FromBody] UpdateMeetingRoomRequest updateMeetingRoomRequest) {
+            var meetingRoom = await _context.MeetingRooms.FindAsync(id);
 
             if(meetingRoom == null){
                 return NotFound();
@@ -57,7 +58,7 @@ namespace api.Controllers
 
             // Update the meeting room with the new values
             _context.Entry(meetingRoom).CurrentValues.SetValues(updateMeetingRoomRequest);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
 
             return Ok(meetingRoom);
         }
